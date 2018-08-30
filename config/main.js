d42cc6dce700a1c7d8f184572e090ffa5556951e
Eugen Steffen <stalepresh121@outlook.com>
2018-08-30 23:57:32 +1000

    beta2

--- config/main.js
+++ config/main.js
@@ -16,7 +16,7 @@ module.exports = {
       ropsten: process.env.DB_CONNECTION || 'mongodb://localhost:27017/coinflipdb_ropsten',
       mainnet: process.env.DB_CONNECTION || 'mongodb://localhost:27017/coinflipdb'
     },
-    app_contract_address: process.env.CONTRACT || '0xa388e4c7000ebbef09d3d01f6e07fca9f335a840',
+    app_contract_address: process.env.CONTRACT || '0x6b423a50cd4d346c6991c2e6e61caf6925eced9c',
     domain: process.env.API_DOMAIN || 'http://localhost:3030',
     last_endpoint_version: '0.0.1',
     version: 'v1',
